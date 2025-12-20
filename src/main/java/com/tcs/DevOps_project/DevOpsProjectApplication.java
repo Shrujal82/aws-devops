cbdaeaeda8cbb3b80799a5b39197d2ac4d8d68d7
Shrujal82 <parmar8297shrujal@gmail.com>
2025-12-20 13:03:01 +0530

    Added new files and folder

---
+++ src/main/java/com/tcs/DevOps_project/DevOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.DevOps_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+
+}
