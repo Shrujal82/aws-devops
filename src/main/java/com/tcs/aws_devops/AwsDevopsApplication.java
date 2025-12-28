df6dc88c19eba29f969c535e312a1c366cd10cf6
Shrujal82 <parmar8297shrujal@gmail.com>
2025-12-28 16:12:49 +0530

    updated java

--- src/main/java/com/tcs/aws_devops/AwsDevopsApplication.java
+++ src/main/java/com/tcs/aws_devops/AwsDevopsApplication.java
@@ -2,12 +2,30 @@ package com.tcs.aws_devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AwsDevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsDevopsApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcom to demo project</h1>";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "<h1>This is about page</h1>";
+	}
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "<h1>This is contact page</h1>";
 	}
 
 }
